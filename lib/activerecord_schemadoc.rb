790ace8ec245e9a2792aa7d93b8fe37a25b51b92
lni_T <6626484+lnit@users.noreply.github.com>
2024-07-02 20:59:49 +0900

    Initial commit

---
+++ lib/activerecord_schemadoc.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "activerecord_schemadoc/version"
+
+module ActiverecordSchemadoc
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/activerecord_schemadoc_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ActiverecordSchemadoc do
+  it "has a version number" do
+    expect(ActiverecordSchemadoc::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
